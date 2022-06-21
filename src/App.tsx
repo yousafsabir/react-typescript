d5e5494c4f86be2c549eb9e4bf24268272673936
yousafsabir <yousafsabir430516@gmail.com>
2022-06-21 15:58:19 +0500

    functions and primitives

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,18 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const name: string = "Yousaf";
+    const age: number = 19;
+    const isMarried: boolean = false;
+
+    const aFunction = (name: string): void => {
+        console.log("name:", name);
+    };
 
     return (
         <div className="App">
             <header className="App-header">
-                <p>Hello Vite + React!</p>
-                <p>
-                    <button
-                        type="button"
-                        onClick={() => setCount((count) => count + 1)}
-                    >
-                        count is: {count}
-                    </button>
-                </p>
-                <p>
-                    Edit <code>App.tsx</code> and save to test HMR updates.
-                </p>
-                <p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    <a
-                        className="App-link"
-                        href="https://vitejs.dev/guide/features.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Vite Docs
-                    </a>
-                </p>
+                <button onClick={() => aFunction("Yousaf")}>click</button>
             </header>
         </div>
     );
